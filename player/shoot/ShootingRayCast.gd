3c96477e1c22f78dded6d3a4f8a064f269d8f826
Linko-3D <dbekhouc@gmail.com>
2020-11-02 21:31:28 +0100

    shooting ability

--- player/shoot/ShootingRayCast.gd
+++ player/shoot/ShootingRayCast.gd
@@ -7,6 +7,7 @@ export var max_ammo = 12
 var current_ammo = max_ammo
 
 var impact = "res://player/shoot/Impact.tscn"
+var shell = "res://player/shoot/Shell.tscn"
 
 var mouse_relative_x = 0
 var mouse_relative_y = 0
@@ -18,21 +19,30 @@ func _ready():
 	camera_node = get_tree().get_root().find_node("Camera", true, false)
 
 func _input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion: # Getting the mouse movement for the weapon sway in the physics process
 		mouse_relative_x = event.relative.x
 		mouse_relative_y = event.relative.y
 	
 	if event is InputEventMouseButton:
-		
-		if event.button_index == BUTTON_LEFT and event.pressed:
+		if event.button_index == BUTTON_LEFT and event.pressed: # Shooting
 			if $FireRate.time_left == 0 and current_ammo > 0 and not $ReloadTween.is_active():
 				current_ammo -= 1
+				print(current_ammo)
 				$FireRate.start()
 				$ShootSound.play()
+				
 				# Recoil animation:
 				tween($ShootTween, $Shoulder/Hand, "rotation_degrees:x", 0, 20, 0.075, Tween.TRANS_BACK, Tween.EASE_IN_OUT, 0, true)
+				tween($ShootTween, $Shoulder/Hand, "translation:z", $Shoulder/Hand.translation.z, $Shoulder/Hand.translation.z + 0.1, 0.075, Tween.TRANS_BACK, Tween.EASE_IN_OUT, 0, true)
+				
+				# Camera shake:
 				tween($ShootTween, camera_node, "rotation_degrees:x", 0, 1, 0.075, 0, 2, 0, true)
-
+				
+				var shell_instance = load(shell).instance()
+				get_tree().get_root().add_child(shell_instance)
+				shell_instance.global_transform = $Shoulder/Hand/ShellPosition.global_transform
+				shell_instance.linear_velocity = $Shoulder/Hand/ShellPosition.global_transform.basis.x * 5
+				
 				if is_colliding():
 					var impact_instance = load(impact).instance()
 					get_tree().get_root().add_child(impact_instance)
@@ -43,11 +53,12 @@ func _input(event):
 						get_collider().apply_central_impulse(-get_collision_normal() * 50)
 						impact_instance.hide_bullet()
 
-	if Input.is_key_pressed(KEY_R):
+	if Input.is_key_pressed(KEY_R): # Reloading
 		if current_ammo < max_ammo:
 			if not $ShootTween.is_active() and not $ReloadTween.is_active():
 				tween($ReloadTween, $Shoulder/Hand, "rotation_degrees:x", 0, 360, 1, Tween.TRANS_BACK, Tween.EASE_IN_OUT, 0, false)
 				current_ammo = max_ammo
+				print(current_ammo)
 				$ReloadSound.play()
 
 func _physics_process(delta):
