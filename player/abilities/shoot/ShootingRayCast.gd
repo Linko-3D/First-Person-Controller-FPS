398937101c788560ce87b917f7a8bc380f017f86
Linko-3D <dbekhouc@gmail.com>
2020-11-28 14:50:10 +0100

    tweak

--- player/abilities/shoot/ShootingRayCast.gd
+++ player/abilities/shoot/ShootingRayCast.gd
@@ -63,7 +63,7 @@ func shoot():
 		var volume = 0
 		for i in 5: # Add an echo effect when shooting by delaying the sound and reducing the volume
 			play_sound(shoot_sound, volume, delay)
-			delay += 0.25
+			delay += 0.3
 			volume -= 20
 	else:
 		play_sound(shoot_sound, 0, 0)
