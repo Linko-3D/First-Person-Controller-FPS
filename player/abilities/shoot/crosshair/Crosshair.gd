4bc05191ee5179cb09c7a67fb63a3def67d5ddce
Linko-3D <dbekhouc@gmail.com>
2020-12-01 23:18:39 +0100

    tweak

--- player/abilities/shoot/crosshair/Crosshair.gd
+++ player/abilities/shoot/crosshair/Crosshair.gd
@@ -3,12 +3,12 @@ extends Position2D
 var animated = true
 
 
-var time = 0.05
+var speed = 0.1
 var base_position = 47.5
-var end_position = 81.5
+var end_position = base_position + 34
 
 var player = null
-var speed = Vector3()
+var movement = Vector3()
 
 func _ready():
 	player = get_tree().get_root().find_node("Player", true, false)
@@ -17,22 +17,21 @@ func _process(delta):
 	position = OS.get_window_size() / 2
 	
 	if animated:
-		speed.x = stepify(player.velocity.x, 10)
-		speed.y = stepify(player.velocity.y, 10)
-		speed.z = stepify(player.velocity.z, 10)
+		movement.x = stepify(player.velocity.x, 10)
+		movement.y = stepify(player.velocity.y, 10)
+		movement.z = stepify(player.velocity.z, 10)
 		
-		if speed == Vector3():
-			if player.is_on_floor():
+		if player.direction == Vector3() and player.is_on_floor():
 				if $Line1.position.x == -end_position and not $Tween.is_active():
-					$Tween.interpolate_property($Line1, "position:x", -end_position, -base_position, time, 0, 2, time)
-					$Tween.interpolate_property($Line2, "position:y", -end_position, -base_position, time, 0, 2, time)
-					$Tween.interpolate_property($Line3, "position:x", end_position, base_position, time, 0, 2, time)
-					$Tween.interpolate_property($Line4, "position:y", end_position, base_position, time, 0, 2, time)
+					$Tween.interpolate_property($Line1, "position:x", -end_position, -base_position, speed, 0, 2, speed)
+					$Tween.interpolate_property($Line2, "position:y", -end_position, -base_position, speed, 0, 2, speed)
+					$Tween.interpolate_property($Line3, "position:x", end_position, base_position, speed, 0, 2, speed)
+					$Tween.interpolate_property($Line4, "position:y", end_position, base_position, speed, 0, 2, speed)
 					$Tween.start()
 		else:
 			if $Line1.position.x == -base_position and not $Tween.is_active():
-				$Tween.interpolate_property($Line1, "position:x", -base_position, -end_position, time, 0, 2, 0)
-				$Tween.interpolate_property($Line2, "position:y", -base_position, -end_position, time, 0, 2, 0)
-				$Tween.interpolate_property($Line3, "position:x", base_position, end_position, time, 0, 2, 0)
-				$Tween.interpolate_property($Line4, "position:y", base_position, end_position, time, 0, 2, 0)
+				$Tween.interpolate_property($Line1, "position:x", -base_position, -end_position, speed, 0, 2, 0)
+				$Tween.interpolate_property($Line2, "position:y", -base_position, -end_position, speed, 0, 2, 0)
+				$Tween.interpolate_property($Line3, "position:x", base_position, end_position, speed, 0, 2, 0)
+				$Tween.interpolate_property($Line4, "position:y", base_position, end_position, speed, 0, 2, 0)
 				$Tween.start()
