2ac11ec58fcda0d96240727f6cc084a425e5556d
Linko-3D <dbekhouc@gmail.com>
2021-07-24 01:24:33 +0200

    tweak

--- singletons/PauseScreen.gd
+++ singletons/PauseScreen.gd
@@ -10,18 +10,27 @@ func _ready():
 	hide()
 
 func _input(event):
-	if not Input.is_key_pressed(KEY_ESCAPE):
+	if not Input.is_key_pressed(KEY_ESCAPE) and not Input.is_joy_button_pressed(0, JOY_START):
 		can_press = true
 	
 	if can_press:
-		if Input.is_key_pressed(KEY_ESCAPE):
+		if Input.is_key_pressed(KEY_ESCAPE) or Input.is_joy_button_pressed(0, JOY_START):
 			can_press = false
 			if get_tree().paused:
 				get_tree().quit()
 			get_tree().paused = true
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 			show()
-	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+	
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) or Input.is_joy_button_pressed(0, JOY_XBOX_A):
 		get_tree().paused = false
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		hide()
+
+func _on_Timer_timeout():
+	if $Label.text == "GAME PAUSED":
+		$Label.text = "GAME PAUSED_"
+	else:
+		$Label.text = "GAME PAUSED"
+	
+	$Timer.start()
