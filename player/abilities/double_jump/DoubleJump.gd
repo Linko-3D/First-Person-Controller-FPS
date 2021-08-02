ea46087a45e11367cfaa067beb9906c11647a9dc
Linko-3D <dbekhouc@gmail.com>
2021-08-02 02:49:05 +0200

    tweak

--- player/abilities/double_jump/DoubleJump.gd
+++ player/abilities/double_jump/DoubleJump.gd
@@ -2,7 +2,7 @@ extends Spatial
 
 onready var player = get_tree().get_root().find_node("Player", true, false)
 
-var jump_remaining = 1
+var jumps_remaining = 1
 var can_jump = false
 
 func _physics_process(delta):
@@ -11,11 +11,11 @@ func _physics_process(delta):
 			$Timer.start()
 	else:
 		can_jump = false
-		jump_remaining = 1
+		jumps_remaining = 1
 	
-	if can_jump and jump_remaining > 0:
+	if can_jump and jumps_remaining > 0:
 		if Input.is_key_pressed(KEY_SPACE) or Input.is_joy_button_pressed(0, JOY_XBOX_A):
-			jump_remaining -= 1
+			jumps_remaining -= 1
 			player.jump()
 
 func _on_Timer_timeout():
