80afd2c8b20a21286245940a1f3827a04bc2a5bb
Linko-3D <dbekhouc@gmail.com>
2021-10-24 16:11:34 +0200

    tweak

---
+++ singletons/PauseScreen.gd
@@ -0,0 +1,40 @@
+extends Control
+
+# Singleton
+
+var can_press = true
+
+func _ready():
+	pause_mode = PAUSE_MODE_PROCESS # This script can't get paused
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	hide()
+	
+	$Control/Label.margin_top = get_viewport().size.y / 4
+	get_tree().connect("screen_resized", self, "_on_screen_resized")
+
+func _process(delta):
+	if not $Tween.is_active():
+		$Tween.interpolate_property($Control/Label, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0.5), 0.4, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+		$Tween.interpolate_property($Control/Label, "modulate", Color(1, 1, 1, 0.5), Color(1, 1, 1, 1), 0.4, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 0.4)
+		$Tween.start()
+
+func _input(event):
+	if not Input.is_key_pressed(KEY_ESCAPE) and not Input.is_joy_button_pressed(0, JOY_START):
+		can_press = true
+	
+	if can_press:
+		if Input.is_key_pressed(KEY_ESCAPE) or Input.is_joy_button_pressed(0, JOY_START):
+			can_press = false
+			if get_tree().paused:
+				get_tree().quit()
+			get_tree().paused = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			show()
+	
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) or Input.is_key_pressed(KEY_SPACE) or Input.is_joy_button_pressed(0, JOY_XBOX_A) or Input.get_joy_axis(0, 7) >= 0.6:
+		get_tree().paused = false
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		hide()
+
+func _on_screen_resized():
+	$Label.margin_top = get_viewport().size.y / 4
