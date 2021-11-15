7b08e5d9886ad9edb83f0200eb08d9d8e614972d
Linko-3D <dbekhouc@gmail.com>
2021-11-15 22:18:11 +0100

    tweak

--- player/abilities/shoot/Impact.gd
+++ player/abilities/shoot/Impact.gd
@@ -9,6 +9,9 @@ func _ready():
 	
 	$ImpactSound.pitch_scale = rand_range(0.95, 1.05)
 	
+	$ColorTween.interpolate_property($OmniLight, "light_energy", 0.1, 0, 0.1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	$ColorTween.interpolate_property($OmniLight, "light_indirect_energy", 0.1, 0, 0.1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	
 	$ColorTween.interpolate_property(material, "emission", Color(1, 0.76, 0), Color(1, 0.76, 0), 0.1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
 	$ColorTween.interpolate_property(material, "emission", Color(1, 0.76, 0.1), Color(0, 0, 0), 0.1, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 0.1)
 	$ColorTween.interpolate_property(material, "emission_energy", 16, 0, 0.2, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
@@ -17,7 +20,6 @@ func _ready():
 
 func spawn_debris(throw_force):
 	$Position3D.rotation_degrees.z = rand_range(0, 360)
-	$Position3D/Position3D2.rotation_degrees.x = rand_range(0, 10)
 	
 	var debris_instance = debris.instance()
 	get_tree().get_root().add_child(debris_instance)
