a6d0fb205bb8f0626aa83912b3ab7cddfe7944e9
Linko-3D <dbekhouc@gmail.com>
2022-04-26 14:42:03 +0200

    tweak

--- player/Player.gd
+++ player/Player.gd
@@ -1,7 +1,11 @@
 extends CharacterBody3D
 
-const SPEED = 8.0
-const JUMP_VELOCITY = 6.5
+
+var run_speed = 8.0
+var walk_speed = run_speed / 2
+
+var current_speed = run_speed
+var JUMP_VELOCITY = 6.5
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * 2
 
@@ -24,33 +28,30 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotation.y -= event.relative.x / 1000
-		$Head/Camera3D.rotation.x -= event.relative.y / 1000
-		$Head/Camera3D.rotation.x = clamp( $Head/Camera3D.rotation.x, deg2rad(-90), deg2rad(90) )
+		$Head/LandingAnimation/Camera3D.rotation.x -= event.relative.y / 1000
+		$Head/LandingAnimation/Camera3D.rotation.x = clamp( $Head/LandingAnimation/Camera3D.rotation.x, deg2rad(-90), deg2rad(90) )
 
 		mouse_relative_x = event.relative.x
 		mouse_relative_y = event.relative.y
 
 func _physics_process(delta):
-	if Input.is_key_pressed(KEY_CTRL) or Input.is_joy_button_pressed(0, JOY_BUTTON_B):
-		print("crouch")
-
 	if Input.get_joy_axis(0, 2) < -0.2 or Input.get_joy_axis(0, 2) > 0.2:
 		rotation.y -= deg2rad( Input.get_joy_axis(0, 2) * 4.3 )
 	if Input.get_joy_axis(0, 3) < -0.2 or Input.get_joy_axis(0, 3) > 0.2:
-		$Head/Camera3D.rotation.x -= deg2rad( Input.get_joy_axis(0, 3) * 4.3 )
+		$Head/LandingAnimation/Camera3D.rotation.x -= deg2rad( Input.get_joy_axis(0, 3) * 4.3 )
 
-	if $Head/Camera3D/RayCast3D.is_colliding():
-		$Head/Camera3D/RightHand/LookAt.look_at( $Head/Camera3D/RayCast3D.get_collision_point(), Vector3.UP )
+	if $Head/LandingAnimation/Camera3D/RayCast3D.is_colliding():
+		$Head/LandingAnimation/Camera3D/RightHand/LookAt.look_at( $Head/LandingAnimation/Camera3D/RayCast3D.get_collision_point(), Vector3.UP )
 	else:
-		$Head/Camera3D/RightHand/LookAt.rotation = Vector3()
-	$Head/Camera3D/RightHand/RightHandLookAtLerp.rotation.x = lerp( $Head/Camera3D/RightHand/RightHandLookAtLerp.rotation.x, $Head/Camera3D/RightHand/LookAt.rotation.x, 10 * delta )
-	$Head/Camera3D/RightHand/RightHandLookAtLerp.rotation.y = lerp( $Head/Camera3D/RightHand/RightHandLookAtLerp.rotation.y, $Head/Camera3D/RightHand/LookAt.rotation.y, 10 * delta )
-	$Head/Camera3D/RightHand/RightHandLookAtLerp.rotation.z = lerp( $Head/Camera3D/RightHand/RightHandLookAtLerp.rotation.z, $Head/Camera3D/RightHand/LookAt.rotation.z, 10 * delta )
+		$Head/LandingAnimation/Camera3D/RightHand/LookAt.rotation = Vector3()
+	$Head/LandingAnimation/Camera3D/RightHand/RightHandLookAtLerp.rotation.x = lerp( $Head/LandingAnimation/Camera3D/RightHand/RightHandLookAtLerp.rotation.x, $Head/LandingAnimation/Camera3D/RightHand/LookAt.rotation.x, 10 * delta )
+	$Head/LandingAnimation/Camera3D/RightHand/RightHandLookAtLerp.rotation.y = lerp( $Head/LandingAnimation/Camera3D/RightHand/RightHandLookAtLerp.rotation.y, $Head/LandingAnimation/Camera3D/RightHand/LookAt.rotation.y, 10 * delta )
+	$Head/LandingAnimation/Camera3D/RightHand/RightHandLookAtLerp.rotation.z = lerp( $Head/LandingAnimation/Camera3D/RightHand/RightHandLookAtLerp.rotation.z, $Head/LandingAnimation/Camera3D/RightHand/LookAt.rotation.z, 10 * delta )
 
 	# Hand sway
-	$Head/Camera3D/RightHand/RightHandLookAtLerp/RightHandSway.rotation.y = lerp( $Head/Camera3D/RightHand/RightHandLookAtLerp/RightHandSway.rotation.y, mouse_relative_x / 3000.0, 20 * delta )
-	$Head/Camera3D/RightHand/RightHandLookAtLerp/RightHandSway.rotation.x = lerp( $Head/Camera3D/RightHand/RightHandLookAtLerp/RightHandSway.rotation.x, -mouse_relative_y / 3000.0, 20 * delta )
-	$Head/Camera3D/RightHand/RightHandLookAtLerp/RightHandSway.rotation.z = lerp( $Head/Camera3D/RightHand/RightHandLookAtLerp/RightHandSway.rotation.y, -mouse_relative_x / 1500.0, 20 * delta )
+	$Head/LandingAnimation/Camera3D/RightHand/RightHandLookAtLerp/RightHandSway.rotation.y = lerp( $Head/LandingAnimation/Camera3D/RightHand/RightHandLookAtLerp/RightHandSway.rotation.y, mouse_relative_x / 3000.0, 20 * delta )
+	$Head/LandingAnimation/Camera3D/RightHand/RightHandLookAtLerp/RightHandSway.rotation.x = lerp( $Head/LandingAnimation/Camera3D/RightHand/RightHandLookAtLerp/RightHandSway.rotation.x, -mouse_relative_y / 3000.0, 20 * delta )
+	$Head/LandingAnimation/Camera3D/RightHand/RightHandLookAtLerp/RightHandSway.rotation.z = lerp( $Head/LandingAnimation/Camera3D/RightHand/RightHandLookAtLerp/RightHandSway.rotation.y, -mouse_relative_x / 1500.0, 20 * delta )
 
 	# Add the gravity
 	if not is_on_floor():
@@ -63,9 +64,9 @@ func _physics_process(delta):
 			if head_position < 0:
 				print("landing velocity: ", landing_velocity)
 				var tween = create_tween()
-				tween.tween_property($Head, "position:y", clamp(0.75 + head_position, 0.25, 0.75), head_position / -2.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-				tween.tween_property($Head, "position:y", 0.75, head_position / -2.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-				
+				# ici
+				tween.tween_property($Head/LandingAnimation, "position:y", clamp(0.0 + head_position, -0.2, 0.0), head_position / -2.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+				tween.tween_property($Head/LandingAnimation, "position:y", 0.0, head_position / -2.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 				if landing_velocity < -3:
 					$LandSound.play()
 				else:
@@ -89,13 +90,27 @@ func _physics_process(delta):
 	if Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT):
 		input_dir.x = 1
 
+	current_speed = run_speed
+
+	if Input.is_key_pressed(KEY_CTRL) or Input.is_joy_button_pressed(0, JOY_BUTTON_B):
+		$CollisionShape3D.shape.height = lerp($CollisionShape3D.shape.height, 1.4, 10 * delta)
+		$Head.position.y = lerp($Head.position.y, 0.3, 10 * delta)
+		current_speed = walk_speed
+	else:
+		$CollisionShape3D.shape.height = lerp($CollisionShape3D.shape.height, 2, 10 * delta)
+		$Head.position.y = lerp($Head.position.y, 0.75, 10 * delta)
+
+	if Input.is_key_pressed(KEY_SHIFT) or Input.get_joy_axis(0, JOY_AXIS_TRIGGER_LEFT) >= 0.6:
+		current_speed = walk_speed
+		
+
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
 
