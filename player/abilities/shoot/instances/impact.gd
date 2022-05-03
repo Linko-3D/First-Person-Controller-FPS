b43587bbd53d0a5c031ad63d9d2a86b02cfcfcad
Linko-3D <dbekhouc@gmail.com>
2022-05-04 01:42:49 +0200

    tweak

--- player/abilities/shoot/instances/impact.gd
+++ player/abilities/shoot/instances/impact.gd
@@ -8,15 +8,15 @@ func _ready():
 	var tween = create_tween()
 	tween.tween_property( $Bullet, "scale", Vector3(1, 1, 1), 0.1 ).set_trans(Tween.TRANS_SINE)
 	tween.tween_property( $Bullet, "scale", Vector3(), 0.1 ).set_trans(Tween.TRANS_SINE)
-	tween.tween_callback(queue_free).set_delay(0.8)
+	tween.tween_callback(queue_free).set_delay(1)
 
 	$OmniLight3D.omni_range = 0
 	$OmniLight3D.light_energy = 0
 	var light_tween = create_tween()
 	light_tween.tween_property( $OmniLight3D, "position:z", 0.5, 0.3 ).set_trans(Tween.TRANS_SINE)
-	light_tween.set_parallel().tween_property( $OmniLight3D, "omni_range", 2.0, 0.15 ).set_trans(Tween.TRANS_SINE)
-	light_tween.set_parallel().tween_property( $OmniLight3D, "omni_range", 0.0, 0.15 ).set_trans(Tween.TRANS_SINE).set_delay(0.15)
-	light_tween.set_parallel().tween_property( $OmniLight3D, "light_energy", 0.5, 0.15 ).set_trans(Tween.TRANS_SINE)
-	light_tween.set_parallel().tween_property( $OmniLight3D, "light_energy", 0.0, 0.15 ).set_trans(Tween.TRANS_SINE).set_delay(0.15)
-	
-#	$Tween.interpolate_property($OmniLight, "translation:z", 0, 0.5, 0.3, Tween.TRANS_SINE, Tween.EASE_OUT)
+	light_tween.set_parallel().tween_property( $OmniLight3D, "omni_range", 2.0, 0.1 ).set_trans(Tween.TRANS_SINE)
+	light_tween.set_parallel().tween_property( $OmniLight3D, "omni_range", 0.0, 0.1 ).set_trans(Tween.TRANS_SINE).set_delay(0.15)
+	light_tween.set_parallel().tween_property( $OmniLight3D, "light_energy", 0.5, 0.1 ).set_trans(Tween.TRANS_SINE)
+	light_tween.set_parallel().tween_property( $OmniLight3D, "light_energy", 0.0, 0.1 ).set_trans(Tween.TRANS_SINE).set_delay(0.15)
+	light_tween.tween_callback($OmniLight3D.queue_free).set_delay(0.2)
+
