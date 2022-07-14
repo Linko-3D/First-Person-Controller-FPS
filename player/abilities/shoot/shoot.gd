ed0d4bc8d177b5196bf8b0843d06e13fd981e92c
Linko-3D <dbekhouc@gmail.com>
2022-07-14 12:21:56 +0200

    tweak

--- player/abilities/shoot/shoot.gd
+++ player/abilities/shoot/shoot.gd
@@ -1,25 +1,30 @@
 extends Position3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var ammo = 30
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) or Input.get_joy_axis(0, JOY_AXIS_TRIGGER_RIGHT) >= 0.6:
 		if $FireRateTimer.is_stopped():
-			print("shoot")
 			$FireRateTimer.start()
-			
+
 			$TriggerSound.pitch_scale = randf_range(0.95, 1.05)
 			$TriggerSound.play()
 			
-			$ShootSound.pitch_scale = randf_range(0.95, 1.05)
-			$ShootSound.play()
+			if ammo > 0:
+				ammo -= 1
+				print(ammo)
+				$AmmoLeft.size.x = ammo
 
-			$BangSound.pitch_scale = randf_range(0.95, 1.05)
-			$BangSound.play()
-			
-			$ShellSound.pitch_scale = randf_range(0.95, 1.05)
-			$ShellSound.play()
+				$ShootSound.pitch_scale = randf_range(0.95, 1.05)
+				$ShootSound.play()
+
+				$BangSound.pitch_scale = randf_range(0.95, 1.05)
+				$BangSound.play()
+
+				$ShellSound.pitch_scale = randf_range(0.95, 1.05)
+				$ShellSound.play()
+
+	if Input.is_key_pressed(KEY_R) or Input.is_joy_button_pressed(0, JOY_BUTTON_X):
+		ammo = 30
+		print(ammo)
+		$AmmoLeft.size.x = ammo
