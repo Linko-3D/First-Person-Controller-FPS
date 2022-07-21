697488046a34380765a2fe17d49ce483f49d5bbb
Linko-3D <dbekhouc@gmail.com>
2022-07-22 00:39:40 +0200

    tweak

--- player/abilities/shoot/shoot.gd
+++ player/abilities/shoot/shoot.gd
@@ -21,6 +21,8 @@ func _process(delta):
 	if Input.is_key_pressed(KEY_R) or Input.is_joy_button_pressed(0, JOY_BUTTON_X):
 		if ammo != 30:
 			$ReloadTimer.start()
+			$AmmoLeft.show()
+			$AmmoLeft.color = Color.BLACK
 			ammo = 30
 			print("ammo: "  , ammo)
 
@@ -29,10 +31,10 @@ func _process(delta):
 
 			var tween = create_tween()
 			$AmmoLeft.size.x = 0
-			$Position3D/Weapon.rotation.x = 0
 			tween.tween_property( $AmmoLeft, "size:x", float(ammo), 1.0 )
 			tween.parallel().tween_property( $Position3D/Weapon, "rotation:x", deg2rad(360), 1 ).set_trans(Tween.TRANS_BACK)
-			
+			tween.parallel().tween_callback($AmmoLeft.show).set_delay(0.1)
+			tween.tween_property( $Position3D/Weapon, "rotation:x", deg2rad(0), 0 )
 
 func shoot():
 	if not $ReloadTimer.is_stopped():
@@ -45,7 +47,25 @@ func shoot():
 	$TriggerSound.play()
 
 	if ammo <= 0:
+		$AmmoLeft.size.x = 30
+		$AmmoLeft.color = Color.RED
+
+		var tween = create_tween()
+		tween.tween_callback($AmmoLeft.show)
+		tween.tween_callback($AmmoLeft.hide).set_delay(0.1)
+
 		return
+
+	var tween = create_tween()
+	tween.tween_property( $Position3D/Weapon, "position:z", randf_range(0.035, 0.045), 0.05 ).set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property( $Position3D/Weapon, "position:y", randf_range(0.005, 0.015), 0.05 ).set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property( $Position3D/Weapon, "position:x", randf_range(-0.005, 0.005), 0.05 ).set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property( $Position3D/Weapon, "rotation:x", deg2rad( randf_range(-1.5, -0.5) ), 0.05 ).set_trans(Tween.TRANS_SINE)
+
+	tween.tween_property( $Position3D/Weapon, "position:z", 0.0, 0.15 ).set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property( $Position3D/Weapon, "position:y", 0.0, 0.15 ).set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property( $Position3D/Weapon, "position:x", 0.0, 0.15 ).set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property( $Position3D/Weapon, "rotation:x", 0.0, 0.15 ).set_trans(Tween.TRANS_SINE)
 
 	ammo -= 1
 	print("ammo: "  , ammo)
