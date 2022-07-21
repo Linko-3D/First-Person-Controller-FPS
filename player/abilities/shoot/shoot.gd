a78e9541a505378af81417988a952348356b090e
Linko-3D <dbekhouc@gmail.com>
2022-07-21 20:06:31 +0200

    tweak

--- player/abilities/shoot/shoot.gd
+++ player/abilities/shoot/shoot.gd
@@ -11,11 +11,21 @@ func _process(delta):
 
 	if Input.is_key_pressed(KEY_R) or Input.is_joy_button_pressed(0, JOY_BUTTON_X):
 		if ammo != 30:
+			$ReloadTimer.start()
 			ammo = 30
 			print("ammo: "  , ammo)
-			$AmmoLeft.size.x = ammo
+			
+			$ReloadSound.pitch_scale = randf_range(0.95, 1.05)
+			$ReloadSound.play()
+			
+			var tween = create_tween()
+			$AmmoLeft.size.x = 0
+			tween.tween_property( $AmmoLeft, "size:x", float(ammo), 1.0 )
+			
 
 func shoot():
+	if not $ReloadTimer.is_stopped():
+		return
 	if not $FireRateTimer.is_stopped():
 		return
 		
@@ -45,4 +55,7 @@ func shoot():
 		get_tree().get_root().add_child(impact_instance)
 		impact_instance.position = $RayCast3D.get_collision_point()
 		impact_instance.rotation = global_transform.basis.get_euler()
+		
+		if $RayCast3D.get_collider() is RigidDynamicBody3D:
+			$RayCast3D.get_collider().apply_central_impulse( -$RayCast3D.get_collision_normal() * 20 )
 #		impact_instance.look_at( $RayCast3D.get_collision_point() - $RayCast3D.get_collision_normal(), Vector3.UP )
